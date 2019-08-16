e114706cdbeace6f0201bee3b72d6ffaa5e469de
barbara-chaves <barbarabarcellos90@gmail.com>
2019-08-16 20:10:36 +0200

    fix placeholder img

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,16 +1,16 @@
 /* eslint-disable indent */
-"use strict";
+'use strict';
 
-const radioBtns = document.querySelectorAll("input[name=cards-number]");
-const startBtn = document.querySelector("#start-btn");
-const cardsContainer = document.querySelector(".cards__list");
+const radioBtns = document.querySelectorAll('input[name=cards-number]');
+const startBtn = document.querySelector('#start-btn');
+const cardsContainer = document.querySelector('.cards__list');
 let cards = [];
-let gameSize = localStorage.getItem("game-size");
+let gameSize = localStorage.getItem('game-size');
 let saveData = [];
 
 
 const getGameSizeFromLocal = () => {
-	gameSize = localStorage.getItem("game-size");
+	gameSize = localStorage.getItem('game-size');
 	for (const radio of radioBtns) {
     if (radio.value === gameSize) {
       radio.checked = true;
@@ -23,28 +23,28 @@ const getGameSizeFromInput = () => {
   for (const radio of radioBtns) {
     if (radio.checked) {
       gameSize = parseInt(radio.value);
-      localStorage.setItem("game-size", gameSize);
+      localStorage.setItem('game-size', gameSize);
     }
   }
   return gameSize;
 };
 
 const createCard = card => {
-  const frontCard = document.createElement("div");
-  frontCard.classList.add("hidden");
-  const frontCardImage = document.createElement("img");
-  frontCardImage.classList.add("card__img");
+  const frontCard = document.createElement('div');
+  frontCard.classList.add('hidden');
+  const frontCardImage = document.createElement('img');
+  frontCardImage.classList.add('card__img');
   frontCardImage.src = card.image;
   frontCard.appendChild(frontCardImage);
 
-  const backCard = document.createElement("div");
-  backCard.classList.add("back-card", "hidden");
-  const backCardImage = document.createElement("img");
-  backCardImage.src = "../assets/images/logo-adalab-80px.png";
+  const backCard = document.createElement('div');
+  backCard.classList.add('back-card', 'hidden');
+  const backCardImage = document.createElement('img');
+  backCardImage.src = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
   backCard.appendChild(backCardImage);
 
-  const newCard = document.createElement("li");
-  newCard.classList.add("card");
+  const newCard = document.createElement('li');
+  newCard.classList.add('card');
   newCard.dataset.pair = card.pair;
   newCard.appendChild(frontCard);
   newCard.appendChild(backCard);
@@ -58,14 +58,14 @@ const printCards = () => {
 };
 
 const turnCard = event => {
-  event.currentTarget.firstElementChild.classList.toggle("front-card");
-  event.currentTarget.lastElementChild.classList.toggle("back-card");
+  event.currentTarget.firstElementChild.classList.toggle('front-card');
+  event.currentTarget.lastElementChild.classList.toggle('back-card');
 };
 
 const addEventOnCards = () => {
-  cards = document.querySelectorAll(".card");
+  cards = document.querySelectorAll('.card');
   for (const card of cards) {
-    card.addEventListener("click", handleCardClick);
+    card.addEventListener('click', handleCardClick);
   }
 };
 
@@ -89,7 +89,7 @@ const generateCards = () => {
   getDataFromServer();
 };
 
-const cleanCardsContainer = () => (cardsContainer.innerHTML = "");
+const cleanCardsContainer = () => (cardsContainer.innerHTML = '');
 
 const startGame = () => {
 	if (gameSize){
@@ -108,28 +108,28 @@ const handleStartBtnClick = () => {
 
 startGame();
 
-startBtn.addEventListener("click", handleStartBtnClick);
+startBtn.addEventListener('click', handleStartBtnClick);
 
 /// Implementation
 
 const turnDontMatchingCards = card => {
-  card.firstElementChild.classList.toggle("front-card");
-  card.lastElementChild.classList.toggle("back-card");
-  card.dataset.face = "back";
+  card.firstElementChild.classList.toggle('front-card');
+  card.lastElementChild.classList.toggle('back-card');
+  card.dataset.face = 'back';
 };
 
 let turnedCards = [];
 const compareCards = () => {
   turnedCards = [];
   for (let i = 0; i < cards.length; i++) {
-    if (cards[i].dataset.face === "front") {
+    if (cards[i].dataset.face === 'front') {
       turnedCards.push(cards[i]);
       if (turnedCards.length === 2) {
         if (turnedCards[0].dataset.pair === turnedCards[1].dataset.pair) {
           for (const card of turnedCards) {
-            card.classList.add("match-pair");
-            card.dataset.face = "blocked";
-            card.removeEventListener("click", handleCardClick);
+            card.classList.add('match-pair');
+            card.dataset.face = 'blocked';
+            card.removeEventListener('click', handleCardClick);
           }
         } else {
           for (const card of turnedCards) {
@@ -143,9 +143,9 @@ const compareCards = () => {
 };
 
 const changeDafaFace = event => {
-  if (event.currentTarget.dataset.face === "front") {
-    event.currentTarget.dataset.face = "back";
+  if (event.currentTarget.dataset.face === 'front') {
+    event.currentTarget.dataset.face = 'back';
   } else {
-    event.currentTarget.dataset.face = "front";
+    event.currentTarget.dataset.face = 'front';
   }
 };
